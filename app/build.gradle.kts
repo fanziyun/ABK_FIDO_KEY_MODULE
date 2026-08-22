ed5e4c02c9e258906d63206407a539b405f9a312
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2026-08-22 17:09:23 +0000

    Use Material pairing dialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("com.github.topjohnwu.libsu:core:5.2.2")
